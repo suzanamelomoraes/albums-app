d8e273ed0848677a69614164c70f7401fd567a71
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-10-04 00:24:33 +1300

    call a function that receives the data from the api, fetches the local store and return it to display each item in a list

--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as api from '../api/albums';
 
 const Albums = () => {
+  const [albums, setAlbumns] = useState([]);
+
+  useEffect(() => {
+    const getAlbums = async () => {
+      try {
+        const res = await api.getAlbums();
+        setAlbumns(res.data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    getAlbums();
+  }, []);
+
   return (
     <div>
       <h1>Albums</h1>
       <ul data-testid='albums-list'></ul>
+      {albums.map((album) => (
+        <li key={album.id} className='albums-item' data-testid='album-item'>
+          {album.title}
+        </li>
+      ))}
     </div>
   );
 };
