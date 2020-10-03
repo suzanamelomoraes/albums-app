4b0883c78dab34e978ed9f11e490c5ac2c4a5560
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-10-04 00:01:03 +1300

    create a test if albums renders an empty ul

--- src/components/Albums.test.js
+++ src/components/Albums.test.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import axios from 'axios';
 import Albums from './Albums';
 
 describe('Albums component works correctly', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   test('Should render Albums component', () => {
     const AlbumsComponent = render(<Albums />);
     expect(AlbumsComponent).toBeTruthy();
+  });
+
+  const fakeAlbumsData = [
+    {
+      title: 'Fake album 1',
+    },
+    {
+      title: 'Fake album 2',
+    },
+    {
+      title: 'Fake album 3',
+    },
+  ];
+
+  const resp = { data: fakeAlbumsData };
+
+  test('Should render a list of Albums titles if data is fetched', async () => {
+    axios.get.mockResolvedValueOnce(resp);
+
+    render(<Albums />);
+
+    const listAlbumElement = screen.getByTestId('albums-list');
+    expect(listAlbumElement).toBeEmpty();
   });
 });
