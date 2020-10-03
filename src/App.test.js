dace55bd3a0e90d3255c7998daa168ac047abf3c
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-10-03 23:30:10 +1300

    create a test to verify if App component renders Albums component

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('Renders <App /> component and its child correctly', () => {
   test('Should render App component', () => {
     const AppComponent = render(<App />);
     expect(AppComponent).toBeTruthy();
+  });
+
+  test('Should render Albums component', () => {
+    render(<App />);
+    expect(screen.getByText(/Albums/i)).toBeInTheDocument();
   });
 });
