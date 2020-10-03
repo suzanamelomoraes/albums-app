7f1d63a340ee318928fb28d271641b1eec248ede
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-10-04 01:00:51 +1300

    write a test to chech if Album component has an empty ul

--- src/components/Album.test.js
+++ src/components/Album.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import axios from 'axios';
 import Album from './Album';
 
 describe('Albums component works correctly', () => {
@@ -10,5 +11,28 @@ describe('Albums component works correctly', () => {
   test('Should render Album component', () => {
     const AlbumComponent = render(<Album />);
     expect(AlbumComponent).toBeTruthy();
+  });
+
+  const fakePhotosAlbumData = [
+    {
+      thumbnailUrl: 'https://via.placeholder.com/150/315aa6',
+    },
+    {
+      thumbnailUrl: 'https://via.placeholder.com/150/1b9d08',
+    },
+    {
+      thumbnailUrl: 'https://via.placeholder.com/150/6dd9cb',
+    },
+  ];
+
+  const resp = { data: fakePhotosAlbumData };
+
+  test('Should render a list of Albums titles if data is fetched', async () => {
+    axios.get.mockResolvedValueOnce(resp);
+
+    render(<Album />);
+
+    const listAlbumElement = screen.getByTestId('albums-list');
+    expect(listAlbumElement).toBeEmpty();
   });
 });
