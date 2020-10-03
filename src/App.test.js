def0f46fe45105a2741f4b395d0021268389cca9
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-10-03 23:26:57 +1300

    create a test to verify if App component is rendering

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Renders <App /> component and its child correctly', () => {
+  test('Should render App component', () => {
+    const AppComponent = render(<App />);
+    expect(AppComponent).toBeTruthy();
+  });
 });
