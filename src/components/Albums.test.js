8fc5e3dc29455b5701a5fe4e20079ae805a29c84
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-10-03 23:44:33 +1300

    create a test to verify if albums component renders correctly

--- src/components/Albums.test.js
+++ src/components/Albums.test.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Albums from './Albums';
 
+describe('Albums component works correctly', () => {
+  test('Should render Albums component', () => {
+    const AlbumsComponent = render(<Albums />);
+    expect(AlbumsComponent).toBeTruthy();
+  });
+});
