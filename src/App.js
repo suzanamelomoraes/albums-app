64bd4f7e7f26ec74c0d205ddcb992ec126e7172c
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-10-03 23:43:56 +1300

    import and return Albums component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Albums from './components/Albums';
 
 function App() {
   return (
     <div>
-      <h1>Albums</h1>
+      <Albums />
     </div>
   );
 }
