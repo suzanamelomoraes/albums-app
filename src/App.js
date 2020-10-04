555fac23c7b34273533e25ad163b5bcf5ddbcbd1
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-10-04 20:39:44 +1300

    import and return Router from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Albums from './components/Albums';
 
 function App() {
   return (
     <div>
-      <Albums />
+      <Router>
+        <Albums />
+      </Router>
     </div>
   );
 }
