e8b08cb0bd7bc831dfa312f9b98f345ecb4ed7ab
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-10-04 21:14:20 +1300

    import and apply Link from react-router-dom

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>
-      <h1>Albums' Store</h1>
-      <p>Home</p>
+    <div className='header'>
+      <h1>Albums' App</h1>
+      <p>
+        <Link className='home' to={'/'}>
+          Home
+        </Link>
+      </p>
     </div>
   );
 };
